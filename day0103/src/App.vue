5f4f9221bd46e45eec610a1d52a59b7511d28729
tong <3170666394@qq.com>
2020-01-03 21:29:55 +0800

    final

---
+++ day0103/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div v-for="item in data" :key="item.id">
+      {{item.num}}
+      <HelloWorld @msg="insreace" :nums="item.num" :ids="item.id"></HelloWorld>
+    </div>
+    {{jishuhe}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  // eslint-disable-next-line space-before-function-paren
+  data() {
+    return {
+      data: [
+        {
+          id: Date.now(),
+          num: 2,
+          name: 'produce1'
+        },
+        {
+          id: Date.now() + 1,
+          num: 3,
+          name: 'produce2'
+        },
+        {
+          id: Date.now() + 2,
+          num: 5,
+          name: 'produce3'
+        }
+      ]
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    // eslint-disable-next-line space-before-function-paren
+    insreace({ num, id }) {
+      this.data.forEach(item => {
+        // eslint-disable-next-line eqeqeq
+        if (item.id == id) {
+          item.num = num
+        }
+      })
+    }
+  },
+  computed: {
+    // eslint-disable-next-line space-before-function-paren
+    jishuhe() {
+      var a = 0
+      this.data.forEach(item => {
+        a += item.num
+      })
+      return a
+    }
+  }
+}
+</script>
---
+++ day0103/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="inc">+</button>
+    <button @click="xiao">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nums: {
+      type: Number
+    },
+    ids: Number
+  },
+  methods: {
+    // eslint-disable-next-line space-before-function-paren
+    inc() {
+      var shuzhi = this.nums + 1
+      this.$emit('msg', { num: shuzhi, id: this.ids })
+    },
+    // eslint-disable-next-line space-before-function-paren
+    xiao() {
+      var shuzhi = null
+      if (this.nums < 1) {
+        shuzhi = 0
+      } else {
+        shuzhi = this.nums - 1
+      }
+      this.$emit('msg', { num: shuzhi, id: this.ids })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
